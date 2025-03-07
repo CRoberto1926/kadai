71ac2ae7c856a98dac12ed672ed10a108a928430
CRoberto1926 <3205022+CRoberto1926@users.noreply.github.com>
2025-03-07 12:29:51 +0100

    Migrate v2 to standalone

--- web/src/app/v2/task-routing/components/routing-upload/routing-upload.component.ts
+++ web/src/app/v2/task-routing/components/routing-upload/routing-upload.component.ts
@@ -20,12 +20,13 @@ import { Component, OnInit } from '@angular/core';
 import { RoutingUploadService } from '@task-routing/services/routing-upload.service';
 import { NotificationService } from 'app/shared/services/notifications/notification.service';
 import { HotToastService } from '@ngneat/hot-toast';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'kadai-routing-upload',
   templateUrl: './routing-upload.component.html',
   styleUrls: ['./routing-upload.component.scss'],
-  standalone: false
+  imports: [NgIf]
 })
 export class RoutingUploadComponent implements OnInit {
   file: File | null = null;
